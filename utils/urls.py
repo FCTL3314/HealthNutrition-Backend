3ed7fd0ac62b33b2a14a65a4812d1701791cd61d
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-06-25 18:12:16 +0300

    Fix spelling error

--- utils/urls.py
+++ utils/urls.py
@@ -1,7 +1,7 @@
 def get_referer_or_default(request, default: str | None = "/") -> str:
     """
     Returns the url from which the user went to this page,
-    if there is no such url, then returns dafault argument.
+    if there is no such url, then returns default argument.
     """
     referer = request.META.get("HTTP_REFERER")
     return referer if referer else default
