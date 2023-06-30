42809ea1cbf2c10332933fc858532e9ea4cba274
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-06-30 12:38:06 +0300

    Update refresh token life time

--- core/settings/base.py
+++ core/settings/base.py
@@ -209,7 +209,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
