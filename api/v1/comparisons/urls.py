ca71dbe4b1f360888c3cdba3d14d9985c9e72aad
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-06-30 14:36:41 +0300

    ComparisonProductGenericViewSet api required product_type_slug for response

--- api/v1/comparisons/urls.py
+++ api/v1/comparisons/urls.py
@@ -1,14 +1,14 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from api.v1.comparisons.views import ComparisonProductModelViewSet, ComparisonProductTypeModelViewSet
+from api.v1.comparisons.views import ComparisonProductGenericViewSet, ComparisonProductTypeModelViewSet
 
 app_name = "comparisons"
 
 router = routers.DefaultRouter()
 
 router.register("product-types", ComparisonProductTypeModelViewSet, basename="product-type-comparisons")
-router.register("products", ComparisonProductModelViewSet, basename="product-comparisons")
+router.register("products", ComparisonProductGenericViewSet, basename="product-comparisons")
 
 urlpatterns = [
     path("", include(router.urls)),
--- api/v1/comparisons/views.py
+++ api/v1/comparisons/views.py
@@ -1,11 +1,18 @@
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.mixins import ListModelMixin
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
 from api.v1.products.paginators import ProductPageNumberPagination, ProductTypePageNumberPagination
-from api.v1.products.serializers import ProductModelSerializer, ProductTypeModelSerializer
-from products.models import Product
+from api.v1.products.serializers import ProductTypeModelSerializer, ProductModelSerializer
+from comparisons.models import Comparison
+from products.models import ProductType
 
+
+# TODO: Rewrite
 
 class ComparisonProductTypeModelViewSet(ModelViewSet):
     permission_classes = (IsAuthenticated,)
@@ -19,12 +26,20 @@ class ComparisonProductTypeModelViewSet(ModelViewSet):
         return queryset.order_by(*settings.PRODUCT_TYPES_ORDERING)
 
 
-class ComparisonProductModelViewSet(ModelViewSet):
+class ComparisonProductGenericViewSet(GenericViewSet, ListModelMixin):
+    model = Comparison
     permission_classes = (IsAuthenticated,)
     serializer_class = ProductModelSerializer
     pagination_class = ProductPageNumberPagination
     lookup_field = "slug"
 
-    def get_queryset(self):
-        initial_queryset = Product.objects.filter(user=self.request.user)
-        return initial_queryset.order_by(*settings.PRODUCTS_ORDERING)
+    def list(self, request, *args, **kwargs):
+        slug = self.request.query_params.get("product_type_slug")
+        if not slug:
+            return Response({"product_type_slug": "This field is required."}, status=status.HTTP_400_BAD_REQUEST)
+        product_type = get_object_or_404(ProductType, slug=slug)
+        products = product_type.cached_products()
+        queryset = products.filter(user=self.request.user).order_by(*settings.PRODUCTS_ORDERING)
+        paginated_queryset = self.paginate_queryset(queryset)
+        serializer = self.serializer_class(paginated_queryset, many=True)
+        return self.get_paginated_response(serializer.data)
