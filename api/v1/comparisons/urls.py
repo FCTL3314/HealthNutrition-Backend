f07de0911753351bf862155d59c8ec41d93c9ba3
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-06-30 13:55:39 +0300

    First implementation of ComparisonProductModelViewSet view

--- api/v1/comparisons/urls.py
+++ api/v1/comparisons/urls.py
@@ -1,13 +1,14 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from api.v1.comparisons.views import ComparisonProductTypeModelViewSet
+from api.v1.comparisons.views import ComparisonProductModelViewSet, ComparisonProductTypeModelViewSet
 
 app_name = "comparisons"
 
 router = routers.DefaultRouter()
 
 router.register("product-types", ComparisonProductTypeModelViewSet, basename="product-type-comparisons")
+router.register("products", ComparisonProductModelViewSet, basename="product-comparisons")
 
 urlpatterns = [
     path("", include(router.urls)),
--- api/v1/comparisons/views.py
+++ api/v1/comparisons/views.py
@@ -2,8 +2,9 @@ from django.conf import settings
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
-from api.v1.products.paginators import ProductTypePageNumberPagination
-from api.v1.products.serializers import ProductTypeModelSerializer
+from api.v1.products.paginators import ProductPageNumberPagination, ProductTypePageNumberPagination
+from api.v1.products.serializers import ProductModelSerializer, ProductTypeModelSerializer
+from products.models import Product
 
 
 class ComparisonProductTypeModelViewSet(ModelViewSet):
@@ -16,3 +17,14 @@ class ComparisonProductTypeModelViewSet(ModelViewSet):
         product_types = self.request.user.comparison_set.product_types()
         queryset = product_types.product_price_annotation()
         return queryset.order_by(*settings.PRODUCT_TYPES_ORDERING)
+
+
+class ComparisonProductModelViewSet(ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ProductModelSerializer
+    pagination_class = ProductPageNumberPagination
+    lookup_field = "slug"
+
+    def get_queryset(self):
+        initial_queryset = Product.objects.filter(user=self.request.user)
+        return initial_queryset.order_by(*settings.PRODUCTS_ORDERING)
