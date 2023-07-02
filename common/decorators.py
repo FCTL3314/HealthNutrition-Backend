777f596159bcfb9b7d8d766c8a91b7045f58f0f8
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-07-03 00:23:09 +0300

    Add docstring for order_queryset decorator, simplify products serializers

--- common/decorators.py
+++ common/decorators.py
@@ -2,6 +2,9 @@ from functools import wraps
 
 
 def order_queryset(*ordering):
+    """
+    Sorts the queryset returned by the function using the ordering parameter.
+    """
     def outer(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
