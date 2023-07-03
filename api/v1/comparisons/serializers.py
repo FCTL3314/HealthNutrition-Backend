89546d197664e1a125d7784ea95293fe36482f3c
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-07-03 13:20:54 +0300

    Create User serializer, user UserModelSerializer in ComparisonModelSerializer instead of user id

--- api/v1/comparisons/serializers.py
+++ api/v1/comparisons/serializers.py
@@ -1,16 +1,14 @@
 from rest_framework import serializers
 
 from api.v1.products.serializers import ProductModelSerializer
+from api.v1.users.serializers import UserModelSerializer
 from comparisons.models import Comparison
 
 
 class ComparisonModelSerializer(serializers.ModelSerializer):
-    # TODO: UserModelSerializer
+    user = UserModelSerializer(read_only=True)
     product = ProductModelSerializer(read_only=True)
 
     class Meta:
         model = Comparison
         fields = ("user", "product")
-        extra_kwargs = {
-            "user": {"required": False},  # TODO: Remove
-        }
---
+++ api/v1/users/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from users.models import User
+
+
+class UserModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "image",
+            "username",
+            "first_name",
+            "last_name",
+            "about",
+            "email",
+            "is_verified",
+            "slug",
+            "date_joined",
+            "last_login",
+        )
