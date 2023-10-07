247d102273021e712fdbb9954a536555fb5d812b
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-10-07 13:13:14 +0300

    Add workflow DJANGO_SETTINGS_MODULE env variable

--- api/conftest.py
+++ api/conftest.py
@@ -7,51 +7,37 @@ from api.base.time_providers import UTCTimeProvider
 
 @pytest.fixture()
 def user():
-    user = mixer.blend("users.User")
-    yield user
-    user.delete()
+    return mixer.blend("users.User")
 
 
 @pytest.fixture()
 def verified_user():
-    user = mixer.blend("users.User", is_verified=True)
-    yield user
-    user.delete()
+    return mixer.blend("users.User", is_verified=True)
 
 
 @pytest.fixture()
 def unverified_user():
-    user = mixer.blend("users.User", is_verified=False)
-    yield user
-    user.delete()
+    return mixer.blend("users.User", is_verified=False)
 
 
 @pytest.fixture()
 def users():
-    users = mixer.cycle().blend("users.User")
-    yield users
-    users.delete()
+    return mixer.cycle().blend("users.User")
 
 
 @pytest.fixture()
 def store():
-    store = mixer.blend("stores.Store")
-    yield store
-    store.delete()
+    return mixer.blend("stores.Store")
 
 
 @pytest.fixture()
 def product_type():
-    product_type = mixer.blend("products.ProductType")
-    yield product_type
-    product_type.delete()
+    return mixer.blend("products.ProductType")
 
 
 @pytest.fixture()
 def product():
-    product = mixer.blend("products.Product")
-    yield product
-    product.delete()
+    return mixer.blend("products.Product")
 
 
 @pytest.fixture()
