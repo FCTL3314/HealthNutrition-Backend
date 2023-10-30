28753b6f9a71c0cdb126ee91cd35227606f58d30
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-10-30 13:24:10 +0300

    Added is_leaf_node and descendant_count django_mptt attributes to the BaseCommentSerializer like has_replies and replies_count respectively

--- api/v1/comments/serializers.py
+++ api/v1/comments/serializers.py
@@ -10,11 +10,28 @@ from api.v1.users.serializers import UserSerializer
 
 class BaseCommentSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
+    has_replies = serializers.SerializerMethodField(read_only=True)
+    replies_count = serializers.IntegerField(
+        source="get_descendant_count", read_only=True
+    )
+
+    @staticmethod
+    def get_has_replies(obj: BaseCommentModel) -> bool:
+        return not obj.is_leaf_node()
 
     class Meta:
         abstract = True
         model = BaseCommentModel
-        fields = ("id", "author", "text", "created_at", "edited")
+        fields = (
+            "id",
+            "author",
+            "text",
+            "created_at",
+            "edited",
+            "parent_id",
+            "has_replies",
+            "replies_count",
+        )
         read_only_fields = ("created_at", "edited")
 
 
@@ -24,6 +41,12 @@ class ProductCommentSerializer(BaseCommentSerializer):
         write_only=True,
         queryset=Product.objects.all(),
         source="product",
+    )
+    parent_id = serializers.PrimaryKeyRelatedField(
+        queryset=ProductComment.objects.all(),
+        source="parent",
+        allow_null=True,
+        required=False,
     )
 
     class Meta(BaseCommentSerializer.Meta):
@@ -37,6 +60,12 @@ class StoreCommentSerializer(BaseCommentSerializer):
         write_only=True,
         queryset=Store.objects.all(),
         source="store",
+    )
+    parent_id = serializers.PrimaryKeyRelatedField(
+        queryset=StoreComment.objects.all(),
+        source="parent",
+        allow_null=True,
+        required=False,
     )
 
     class Meta(BaseCommentSerializer.Meta):
