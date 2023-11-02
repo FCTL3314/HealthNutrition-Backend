e990a7abb7e14f1ea5de88336cdbed0c48cb8400
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-11-02 14:05:09 +0300

    Added ALLOWED_CONTENT_TYPE_MODEL_NAMES and ALLOWED_CONTENT_TYPES_QUERYSET constant to the Comment model attributes; Added get_absolute_url method.

--- api/v1/comments/models.py
+++ api/v1/comments/models.py
@@ -1,16 +1,16 @@
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.urls import reverse
 from mptt.fields import TreeForeignKey
 from mptt.models import MPTTModel
 
 from api.v1.comments.managers import BaseCommentManager
+from api.v1.products.models import Product
+from api.v1.stores.models import Store
 
 
 class Comment(MPTTModel):
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey("content_type", "object_id")
     author = models.ForeignKey(to="users.User", null=True, on_delete=models.CASCADE)
     text = models.CharField(max_length=516)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -22,8 +22,22 @@ class Comment(MPTTModel):
         related_name="children",
         on_delete=models.CASCADE,
     )
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+
+    ALLOWED_CONTENT_TYPE_MODEL_NAMES = {
+        Product._meta.model_name,
+        Store._meta.model_name,
+    }
+    ALLOWED_CONTENT_TYPES_QUERYSET = ContentType.objects.filter(
+        model__in=ALLOWED_CONTENT_TYPE_MODEL_NAMES
+    )
 
     objects = BaseCommentManager()
 
     def __str__(self):
         return self.text
+
+    def get_absolute_url(self):
+        return reverse("api:v1:comments:comments-detail", kwargs={"pk": self.pk})
