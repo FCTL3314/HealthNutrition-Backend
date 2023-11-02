964424ccf66a27d9f7bb2b18052320736e54b55b
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-11-02 14:01:23 +0300

    Create BaseCommentSerializer, created the CommentSerializer and CommentReadSerializer serializers that inherit the CommentSerializer

--- api/v1/comments/serializers.py
+++ api/v1/comments/serializers.py
@@ -1,15 +1,15 @@
-from django.contrib.contenttypes.models import ContentType
 from rest_framework import serializers
 
 from api.v1.comments.models import Comment
 from api.v1.users.serializers import UserSerializer
 
 
-class CommentSerializer(serializers.ModelSerializer):
-    author = UserSerializer(read_only=True)
-    has_replies = serializers.SerializerMethodField(read_only=True)
-    replies_count = serializers.IntegerField(
-        source="get_descendant_count", read_only=True
+class BaseCommentSerializer(serializers.ModelSerializer):
+    object_id = serializers.IntegerField(required=True)
+    content_type = serializers.SlugRelatedField(
+        queryset=Comment.ALLOWED_CONTENT_TYPES_QUERYSET,
+        slug_field="model",
+        required=True,
     )
     parent_id = serializers.PrimaryKeyRelatedField(
         queryset=Comment.objects.all(),
@@ -17,28 +17,40 @@ class CommentSerializer(serializers.ModelSerializer):
         allow_null=True,
         required=False,
     )
-    object_id = serializers.IntegerField(write_only=True)
-    content_type_id = serializers.PrimaryKeyRelatedField(
-        queryset=ContentType.objects.all(),
-        source="content_type",
+
+    class Meta:
+        abstract = True
+        model = Comment
+        fields = (
+            "parent_id",
+            "object_id",
+            "content_type",
+        )
+
+
+class CommentSerializer(BaseCommentSerializer):
+    author = UserSerializer(read_only=True)
+    has_replies = serializers.SerializerMethodField(read_only=True)
+    replies_count = serializers.IntegerField(
+        source="get_descendant_count", read_only=True
     )
 
     @staticmethod
     def get_has_replies(obj: Comment) -> bool:
         return not obj.is_leaf_node()
 
-    class Meta:
-        model = Comment
-        fields = (
+    class Meta(BaseCommentSerializer.Meta):
+        fields = BaseCommentSerializer.Meta.fields + (
             "id",
             "author",
             "text",
             "created_at",
             "edited",
-            "parent_id",
             "has_replies",
             "replies_count",
-            "object_id",
-            "content_type_id",
         )
         read_only_fields = ("created_at", "edited")
+
+
+class CommentReadSerializer(BaseCommentSerializer):
+    ...
