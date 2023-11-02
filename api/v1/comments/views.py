d716a8466ad58fcea4f0fbcc32edff80da89b704
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-11-02 14:03:31 +0300

    Added CommentReadSerializer to the CommentViewSet's list method, filter queryset by content_type__model.

--- api/v1/comments/views.py
+++ api/v1/comments/views.py
@@ -1,13 +1,13 @@
 from django.db.models import QuerySet
 from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 from rest_framework.serializers import Serializer
 from rest_framework.viewsets import GenericViewSet
 
-from api.v1.comments.filters import CommentFilter
 from api.v1.comments.models import Comment
 from api.v1.comments.paginators import CommentPageNumberPagination
-from api.v1.comments.serializers import CommentSerializer
+from api.v1.comments.serializers import CommentSerializer, CommentReadSerializer
 
 
 class CommentViewSet(
@@ -21,26 +21,21 @@ class CommentViewSet(
     serializer_class = CommentSerializer
     pagination_class = CommentPageNumberPagination
     permission_classes = (IsAuthenticatedOrReadOnly,)
-    filterset_class = CommentFilter
-    filterset_actions = ("list",)
-
-    def filter_queryset(self, queryset):
-        """
-        A wrapper that allows queryset filtering
-        only if the query is sent by the correct
-        method.
-        """
-        if self.action in self.filterset_actions:
-            return super().filter_queryset(queryset)
-        return queryset
 
     def get_queryset(self) -> QuerySet[Comment]:
         queryset = self.model.objects.newest()
         if self.action != "list":
             return queryset
+        queryset = queryset.filter(
+            content_type__model=self.request.query_params.get("content_type")
+        )
         if parent_id := self.request.query_params.get("parent_id"):
             return queryset.filter(parent_id=parent_id)
         return queryset.top_level()
+
+    def list(self, request, *args, **kwargs) -> Response:
+        CommentReadSerializer(data=request.query_params).is_valid(raise_exception=True)
+        return super().list(request, *args, **kwargs)
 
     def perform_create(self, serializer: Serializer) -> None:
         serializer.save(author=self.request.user)
