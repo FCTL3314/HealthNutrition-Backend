1c5b88782826f79b2a7cf5934b304839800814d1
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-11-02 14:05:43 +0300

    Remove deprecated comments fixtures

--- api/v1/comments/tests/conftest.py
+++ api/v1/comments/tests/conftest.py
@@ -1,27 +1,10 @@
 import pytest
 from mixer.backend.django import mixer
 
-from api.v1.comments.constants import COMMENTS_PAGINATE_BY
-
 
 @pytest.fixture()
-def product_comment():
-    return mixer.blend("comments.ProductComment")
-
-
-@pytest.fixture()
-def product_comments():
-    return mixer.cycle(COMMENTS_PAGINATE_BY * 2).blend("comments.ProductComment")
-
-
-@pytest.fixture()
-def store_comment():
-    return mixer.blend("comments.StoreComment")
-
-
-@pytest.fixture()
-def store_comments():
-    return mixer.cycle(COMMENTS_PAGINATE_BY * 2).blend("comments.StoreComment")
+def comment():
+    return mixer.blend("comments.Comment")
 
 
 @pytest.fixture()
