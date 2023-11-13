70ff6366e406f15388daa3c89bb41d7556d68045
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-11-13 15:23:36 +0300

    Add Diet application models to the admin

--- api/v1/diets/admin.py
+++ api/v1/diets/admin.py
@@ -1,1 +1,20 @@
-# Register your models here.
+from django.contrib import admin
+
+from api.v1.diets.models import DietType, Diet, DietProduct
+
+
+class DietProductAdmin(admin.TabularInline):
+    model = DietProduct
+
+
+@admin.register(DietType)
+class DietTypeAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(Diet)
+class DietAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    ordering = ("name",)
+    inlines = (DietProductAdmin,)
