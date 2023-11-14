fb4a6795442b6958d8c7da98c75aad3d077043a6
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-11-14 13:09:18 +0300

    Create NutritionHealthfulnessImportance dataclass, for products healthfulness calculating.

--- api/v1/products/constants.py
+++ api/v1/products/constants.py
@@ -1,4 +1,13 @@
+from dataclasses import dataclass
+
 PRICE_ROUNDING = 2
 PRODUCTS_PAGINATE_BY = 16
 PRODUCTS_ORDERING = ("name",)
 PRODUCT_VIEW_CACHE_TIME = 60 * 60
+
+
+@dataclass(frozen=True)
+class NutritionHealthfulnessImportance:
+    PROTEIN = 3
+    FAT = 2
+    CARBS = 1
