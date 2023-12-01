dbb9493454f934209f855c5f19936e0335043d0b
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-12-01 12:52:47 +0300

    Change comparison urls

--- api/v1/comparisons/urls.py
+++ api/v1/comparisons/urls.py
@@ -1,14 +1,21 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from api.v1.comparisons.views import ComparisonsViewSet, ComparisonGroupViewSet
+from api.v1.comparisons.views import (
+    ComparisonsDestroyView,
+    ComparisonCreateView,
+    ComparisonListView,
+    ComparisonGroupViewSet,
+)
 
 app_name = "comparisons"
 
 router = DefaultRouter()
 router.register("groups", ComparisonGroupViewSet, basename="comparison-groups")
-router.register("", ComparisonsViewSet, basename="comparisons")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("list/", ComparisonListView.as_view(), name="list"),
+    path("add/", ComparisonCreateView.as_view(), name="add"),
+    path("remove/", ComparisonsDestroyView.as_view(), name="remove"),
 ]
