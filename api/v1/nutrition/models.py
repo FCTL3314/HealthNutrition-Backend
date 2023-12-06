300f779d7d1eb90ff6ea895530eef67fe2974eec
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-12-06 15:44:10 +0300

    Add Nutrition indexes

--- api/v1/nutrition/models.py
+++ api/v1/nutrition/models.py
@@ -9,10 +9,24 @@ class Nutrition(models.Model):
 
     class Meta:
         verbose_name_plural = "Nutrition"
+        indexes = (
+            models.Index(
+                fields=("calories",),
+            ),
+            models.Index(
+                fields=("protein",),
+            ),
+            models.Index(
+                fields=("fat",),
+            ),
+            models.Index(
+                fields=("carbs",),
+            ),
+        )
 
     def __str__(self):
         return (
-            f"Calories: {self.calories} g. "
+            f"Calories: {self.calories} kcal "
             f"Protein: {self.protein} g. "
             f"Fat: {self.fat} g. "
             f"Carbs: {self.carbs} g."
