661ca53e3564936bbef4ee5a9e2fa8d0dabe3b6c
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-12-18 12:56:03 +0300

    Added DjangoSummerNote

--- api/v1/products/admin.py
+++ api/v1/products/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 
 from api.v1.comments.admin import CommentInlineAdmin
 from api.v1.products.models import Product
 
 
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(SummernoteModelAdmin):
+    summernote_fields = ("description",)
     search_fields = ("name", "slug")
     ordering = ("name",)
     prepopulated_fields = {"slug": ("name",)}
--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("summernote/", include("django_summernote.urls")),
     path("api/", include("api.urls", namespace="api-v1")),
 ]
 
