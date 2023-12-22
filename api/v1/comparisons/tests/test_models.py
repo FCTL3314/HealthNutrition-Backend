bd36c02c142441dbdbcd38eb31acee5f88c00d54
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-12-22 12:06:27 +0300

    Created test_calculate_new_comparison_group_position test

--- api/v1/comparisons/tests/test_models.py
+++ api/v1/comparisons/tests/test_models.py
@@ -2,6 +2,7 @@ import pytest
 from django.contrib.auth import get_user_model
 
 from api.v1.comparisons.models import Comparison, ComparisonGroup
+from api.v1.comparisons.services.models import calculate_new_comparison_group_position
 from api.v1.comparisons.tests.conftest import create_comparisons_for_comparison_group
 
 User = get_user_model()
@@ -12,9 +13,18 @@ def test_comparison_manager_products(comparison_group: ComparisonGroup):
     comparisons = create_comparisons_for_comparison_group(comparison_group.id)
 
     expected = {comparison.product for comparison in comparisons}
-    actual = Comparison.objects.products(comparison_group.id)
+    actual = Comparison.objects.products(comparison_group.id)  # noqa
 
     assert len(expected) == len(actual)
+
+
+@pytest.mark.django_db
+def test_calculate_new_comparison_group_position(comparison_group: ComparisonGroup):
+    current_group_position = comparison_group.position
+    new_group_position = calculate_new_comparison_group_position(
+        comparison_group.author
+    )
+    assert new_group_position == (current_group_position - 1)
 
 
 if __name__ == "__main__":
