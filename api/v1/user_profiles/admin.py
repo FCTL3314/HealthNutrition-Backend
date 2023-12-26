8ed64e183b585550140ca4b91103c1daba9ea0aa
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-12-26 16:26:51 +0300

    Added UserProfileAdmin

--- api/v1/user_profiles/admin.py
+++ api/v1/user_profiles/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+
+from api.v1.user_profiles.models import UserProfile
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    search_fields = ("about", "body_weight")
