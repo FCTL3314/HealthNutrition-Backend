f9f6c2eee76386e43a42bc8e4500541e1a19572a
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-12-28 12:35:28 +0300

    Added --noinput flags to the entrypoint.sh

--- docker/production/entrypoint.sh
+++ docker/production/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-poetry run python manage.py makemigrations
-poetry run python manage.py migrate
-poetry run python manage.py collectstatic
+poetry run python manage.py makemigrations --noinput
+poetry run python manage.py migrate --noinput
+poetry run python manage.py collectstatic --noinput
 
 poetry run gunicorn core.wsgi:application --bind 0.0.0.0:8000
